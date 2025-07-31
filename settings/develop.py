186642810ca29b98d4f543264bad4ca367d636ff
Desmond Nnebue <sirdesmond09@gmail.com>
2025-08-01 00:13:17 +0100

    fix allowed host

--- settings/develop.py
+++ settings/develop.py
@@ -8,7 +8,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0").split(",")
 
 # CORS Settings for development
 CORS_ALLOW_ALL_ORIGINS = True
